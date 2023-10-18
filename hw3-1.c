ed10db904ef1ef349a1474304a821dfa2c3c9b1a
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-18 23:24:24 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+    int i, j;
+    scanf("%d", &i);
+    for ( j=2 ; i>j ; j++ ) {
+        if ( i%j==0 ) {
+            printf("NO");
+            break;
+        }
+    }
+    if ( i<=j ) {
+        printf("YES");
+    }
+}
