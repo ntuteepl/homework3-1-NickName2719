5ad39b35b89dfd6b57c5bc7f3bdbce83497a8fde
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-18 22:31:22 +0800

    Update hw3-3.c

--- hw3-3.c
+++ hw3-3.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    int a, sa=0, b, sb=0, z;
+    scanf( "%d%d", &a, &b );
+    int ao = a ;
+    int bo = b ;
+    for ( z=10 ; a>(z/10) ; z*=10 ) {
+        int ar = a%z ; //餘數
+        sa = sa + (ar*10/z) ;
+        a -= ar ;
+    }
+    for ( z=10 ; b>(z/10) ; z*=10 ) {
+        int br = b%z ; //餘數
+        sb = sb + (br*10/z) ;
+        b -= br ;
+    }
+    if ( sa < sb ) {
+        printf ( "%d\n", ao );
+    }
+    else if ( sb < sa ) {
+        printf ( "%d\n", bo );
+    }
+    else {
+        if ( a < b ) {
+            printf ( "%d\n", ao ) ;
+        }
+        else if ( b < a ) {
+            printf ( "%d\n", bo ) ;
+        }
+    }
+}
 
