d4f3f5642a82c13a4c67819485b9b356b41e6164
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-18 21:44:35 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 
 int main(){
-    int s, d, s2, d2, i ;
+    int s, d, s2, d2, i, temp ;
     int counter = 1;
     scanf( "%d%d", &s, &d );
     for ( i = 0 ; i < 2 ; i++  ) {
         scanf( "%d%d", &s2, &d2 );
-        if ( d <= s2 ) {
+        if ( s >= s2 ) {
+            temp = s ;
             s = s2 ;
+            s2 = temp ;
+            temp = d ;
+            d = d2 ;
+            d2 = temp ;
+        }
+        if ( d <= s2 ) {
+            d = d2 ;
         }
         else {
             counter++ ;
