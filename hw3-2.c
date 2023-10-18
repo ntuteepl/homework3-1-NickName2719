29346b66ab3b17c1b9bc25c52d037edf5fa7261a
NickName2719 <146065137+NickName2719@users.noreply.github.com>
2023-10-18 21:29:06 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+    int s, d, s2, d2, i ;
+    int counter = 1;
+    scanf( "%d%d", &s, &d );
+    for ( i = 0 ; i < 2 ; i++  ) {
+        scanf( "%d%d", &s2, &d2 );
+        if ( d <= s2 ) {
+            s = s2 ;
+        }
+        else {
+            counter++ ;
+        }
+    }
+    printf("%d\n", counter);
+}
